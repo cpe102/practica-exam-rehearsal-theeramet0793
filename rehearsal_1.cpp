f7733104897f81e3b2170d9316023f50dcb335aa
theeramet0793 <theerametmetha@gmail.com>
2020-01-10 14:48:29 +0700

    commit-1

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
 
+string name;
+float  grade;
+
+cout<<"What is your name?: ";
+cin>>name;
+cout<<"What is your GPA: ";
+cin>>grade;
+
+if(grade>=3.5){
+    cout<<name<<" InW  Jrim Jrim!!!";
+}
+else
+{
+    cout<<"Try harder,"<<name<<"!!!";
+}
+
+
+return 0;
+}
