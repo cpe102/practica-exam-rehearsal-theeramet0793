d21d44d886d0cfcfd8f3169230ce8a3c8042a14d
theeramet0793 <theerametmetha@gmail.com>
2020-01-10 15:17:11 +0700

    commit-1

--- rehearsal_3.cpp
+++ rehearsal_3.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include<cmath>
 
+
+using namespace std;
 //Write sumSqrt() here.
+double sum=0;
+
+double sumSqrt(int N){
+    sum=0;
+    if(N<=0){
+        return 0;
+    }
+    else{
+        while(N>0){
+        sum=sum+(1/pow(N,0.5));
+        N=N-1;
+        }
+     return sum;
+    }
+
+}
 
 
 int main()
@@ -14,5 +33,5 @@ int main()
     double g = sumSqrt(-1);
     
     cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n";
-
+return 0;
 }
