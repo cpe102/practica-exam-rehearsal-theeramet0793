22c8856d40467cc584501d123f0d2a38e9f99d76
theeramet0793 <theerametmetha@gmail.com>
2020-01-10 14:55:53 +0700

    commit-2

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+   float x,sum=0;
+   p1:
+   cout<<"Enter x: ";
+   cin>>x;
+   if (x==0)
+   {
+       cout<<"sum = "<<sum;
+   }
+   else
+   {
+       if(x>0){
+         sum=sum+x;  
+       }
+       goto p1;   
+   }
+   
 
+
+   return 0; 
+}
