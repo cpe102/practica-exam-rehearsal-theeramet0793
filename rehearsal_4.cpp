b575e42177f142646fb28526a5dbcb429d12973c
theeramet0793 <theerametmetha@gmail.com>
2020-01-10 15:42:50 +0700

    commit-2

--- rehearsal_4.cpp
+++ rehearsal_4.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int i;
 
-string conpress(string name){
+string compress(string name){
    i=1;
    string new_name;
    while(i<=name.size()){
@@ -22,15 +22,15 @@ return new_name;
 
 int main()
 {   
-    string a = conpress("ABCDEFGHIJKLMN");
-    string b = conpress("123456");
-    string c = conpress("HelloWorld");
-    string d = conpress("BNK48");
-    string e = conpress("COMPROG261102");
-    string f = conpress("A");
-    string g = conpress("AB");
-    string h = conpress("ABC");
-    string i = conpress("ABCD");
+    string a = compress("ABCDEFGHIJKLMN");
+    string b = compress("123456");
+    string c = compress("HelloWorld");
+    string d = compress("BNK48");
+    string e = compress("COMPROG261102");
+    string f = compress("A");
+    string g = compress("AB");
+    string h = compress("ABC");
+    string i = compress("ABCD");
     
     cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n" << h << "\n" << i << "\n";
 
