866c1d619edd418b374f8d38131cbbf15a5649a4
theeramet0793 <theerametmetha@gmail.com>
2020-01-10 15:32:27 +0700

    commit-1

--- rehearsal_4.cpp
+++ rehearsal_4.cpp
@@ -1,9 +1,27 @@
-#include <iostream>
-
+#include<iostream>
+#include<string>
 //Write compress() here.
+using namespace std;
+
+int i;
+
+string conpress(string name){
+   i=1;
+   string new_name;
+   while(i<=name.size()){
+       if(i%3==1){
+           new_name=new_name+name[i-1];  
+       }
+      i=i+1; 
+   }
+return new_name;
+}
+
+
+
 
 int main()
-{
+{   
     string a = conpress("ABCDEFGHIJKLMN");
     string b = conpress("123456");
     string c = conpress("HelloWorld");
